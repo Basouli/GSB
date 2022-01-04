48d6603a2158e303e334831a1bee6f420ff7fa9a
Basouli <basil.collette@outlook.fr>
2022-01-04 01:00:02 +0100

    Plannified - 2022-1-4

---
+++ bdd_restore/2022-1-/2022-1-4.sql
@@ -0,0 +1,69 @@
+-- MySQL dump 10.13  Distrib 5.7.31, for Win64 (x86_64)
+--
+-- Host: localhost    Database: gsb_frais
+-- ------------------------------------------------------
+-- Server version	5.7.31
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `etat`
+--
+
+DROP TABLE IF EXISTS `etat`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `etat` (
+  `id` char(2) NOT NULL,
+  `libelle` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `etat`
+--
+
+LOCK TABLES `etat` WRITE;
+/*!40000 ALTER TABLE `etat` DISABLE KEYS */;
+INSERT INTO `etat` VALUES ('CL','Saisie clôturée'),('CR','Fiche créée, saisie en cours'),('RB','Remboursée'),('VA','Validée et mise en paiement');
+/*!40000 ALTER TABLE `etat` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `fichefrais`
+--
+
+DROP TABLE IF EXISTS `fichefrais`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `fichefrais` (
+  `idvisiteur` char(4) NOT NULL,
+  `mois` char(6) NOT NULL,
+  `nbjustificatifs` int(11) DEFAULT NULL,
+  `montantvalide` decimal(10,2) DEFAULT NULL,
+  `datemodif` date DEFAULT NULL,
+  `idetat` char(2) DEFAULT 'CR',
+  PRIMARY KEY (`idvisiteur`,`mois`),
+  KEY `idetat` (`idetat`),
+  CONSTRAINT `fichefrais_ibfk_1` FOREIGN KEY (`idetat`) REFERENCES `etat` (`id`),
+  CONSTRAINT `fichefrais_ibfk_2` FOREIGN KEY (`idvisiteur`) REFERENCES `visiteur` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `fichefrais`
+--
+
+LOCK TABLES `fichefrais` WRITE;
+/*!40000 ALTER TABLE `fichefrais` DISABLE KEYS */;
+INSERT INTO `fichefrais` VALUES ('a131','202110',0,0.00,'2021-11-17','CR'),('a131','202111',0,0.00,'2021-11-17','CL'),('a131','202112',0,0.00,'2021-11-17','CR');
